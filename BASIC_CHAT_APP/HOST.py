f89e18e00ec78afe889a90f38698c6d1290860f4
Mainak Roy <75011220+MainakRoyCalcutta@users.noreply.github.com>
2021-02-24 11:18:44 +0530

    Update HOST.py

--- BASIC_CHAT_APP/HOST.py
+++ BASIC_CHAT_APP/HOST.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket()
 host = socket.gethostname()
-print("Your connection server is starting on : ", host)
+print("Your connection server is starting on: ", host)
 port = 1025
 
 s.bind((host, port))
@@ -14,7 +14,7 @@ connection, adr = s.accept()
 print("Connection established to address: ", adr)
 
 while 1:
-    msg = input(str("Enter your Message : "))
+    msg = input(str("Enter your Message: "))
     msg = msg.encode()
     connection.send(msg)
 
